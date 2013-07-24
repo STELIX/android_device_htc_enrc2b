9e9416de1835a9e07f9a9c649ab1de5c322e704d
Rob Shannon <lloir@kanger.me>
2013-07-24 18:27:56 +0100

    4.3

--- ax.mk
+++ ax.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/ax/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := enrc2b
 
 #Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=enrc2b BUILD_FINGERPRINT="htc_europe/enrc2b/enrc2b:4.2.2/JRO03C/125529.22:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.22 CL125529 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=enrc2b BUILD_FINGERPRINT="htc_europe/enrc2b/enrc2b:4.2.3/JSS15J/125529.22:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.22 CL125529 release-keys"
 
 # Device identifier.
 PRODUCT_DEVICE := enrc2b
