447aa6a5d32398b8421aa717ad551914d1bd8814
STELIX PippoX3 <STELIX@users.noreply.github.com>
2013-10-15 16:07:57 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,7 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo ax_enrc2b-userdebug
-add_lunch_combo ax_enrc2b-eng
+add_lunch_combo carbon_enrc2b-userdebug
+add_lunch_combo carbon_enrc2b-eng
+#add_lunch_combo ax_enrc2b-userdebug
+#add_lunch_combo ax_enrc2b-eng
