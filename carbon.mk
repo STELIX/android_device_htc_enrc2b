78649c434f8c4874daf863cefa855c46b82c135a
MURA Stefano <ssspinni@gmail.com>
2013-10-28 18:41:09 +0100

    Updates

--- carbon.mk
+++ carbon.mk
@@ -6,9 +6,8 @@ $(call inherit-product, device/htc/enrc2b/enrc2b.mk)
 PRODUCT_RELEASE_NAME := enrc2b
 
 # Preload bootanimation
-TARGET_BOOTANIMATION_PRELOAD := true
+#TARGET_BOOTANIMATION_PRELOAD := true    device/htc/enrc2b/bootanimation.zip:system/media/bootanimation.zip \
 PRODUCT_COPY_FILES +=  \
-    device/htc/enrc2b/bootanimation.zip:system/media/bootanimation.zip \
         device/htc/enrc2b/audio/alarms/Alarm_Beep_01.ogg:system/media/audio/alarms/Alarm_Beep_01.ogg \
 	device/htc/enrc2b/audio/alarms/Alarm_Beep_02.ogg:system/media/audio/alarms/Alarm_Beep_02.ogg \
 	device/htc/enrc2b/audio/alarms/Alarm_Beep_03.ogg:system/media/audio/alarms/Alarm_Beep_03.ogg \
@@ -114,8 +113,8 @@ PRODUCT_COPY_FILES +=  \
 
 	
 # Boot animation
-#TARGET_SCREEN_HEIGHT := 1280
-#TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
@@ -123,8 +122,9 @@ $(call inherit-product, vendor/carbon/config/common_phone.mk)
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
-PRODUCT_PACKAGES += ShareLogs
-PRODUCT_PACKAGES += Tegra3Parts
+PRODUCT_PACKAGES += ShareLogs \
+                    Tegra3Parts \
+                    Calendar
 
 # Inherit device configuration
 $(call inherit-product, device/htc/enrc2b/full_enrc2b.mk)
@@ -138,4 +138,4 @@ PRODUCT_MANUFACTURER := HTC
 CARBON_BUILDTYPE := NIGHTLY
 
 #Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=enrc2b BUILD_FINGERPRINT="htc_europe/enrc2b/enrc2b:4.3/JLS36I/125529.22:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.22 CL125529 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=carbon_enrc2b BUILD_FINGERPRINT="htc_europe/enrc2b/enrc2b:4.3/JLS36I/125529.22:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.22 CL125529 release-keys"
